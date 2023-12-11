25556f8e64df6ccb1d452467101a47beac122826
Son Seung Gyu <thstmd0705@naver.com>
2023-12-11 13:49:47 +0900

    Iterator를 이용한 map 순회

--- src/map/quiz/Quiz3.java
+++ src/map/quiz/Quiz3.java
@@ -1,6 +1,7 @@
 package map.quiz;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Set;
 
 public class Quiz3 {
@@ -13,6 +14,14 @@ public class Quiz3 {
 		Set<String> keyset = map.keySet();
 		System.out.println(map.keySet());//[국어, 수학, 영어] -> 키들의 집합을 보여준다.
 		
+		Iterator<String> keys = keyset.iterator();
+		while(keys.hasNext()) {
+			String key = keys.next();
+			System.out.println("[과목명]: " + key);
+			System.out.println("[점수]: " + map.get(key));
+		}
+		
+		System.out.println("----------for 이용--------------");
 		for (String key : keyset) {
 			System.out.println("[Key]: " + key);
 		}
